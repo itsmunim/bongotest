2e472c26277ec1722c2ba5db24094a9186647fd6
dibosh <md-abdul_munim@astro.com.my>
2019-01-25 00:00:00 +0800

    Implement method

--- bongotest/depthcheck.py
+++ bongotest/depthcheck.py
@@ -2,5 +2,13 @@ def print_depth(data):
     pass
 
 
-def get_depths(data, initial_depth=0):
-    pass
+def get_depths(data, curr_depth=0):
+    depths = []
+    for k in data:
+        depths.append((k, curr_depth + 1))
+        if type(data[k]) is dict:
+            depths.extend(get_depths(data[k], curr_depth + 1))
+        if hasattr(data[k], '__dict__'):
+            depths.extend(get_depths(data[k].__dict__, curr_depth + 1))
+
+    return depths
