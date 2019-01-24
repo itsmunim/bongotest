2d729e6f29ecaa35c85cc880557e7d19b9cb812e
dibosh <md-abdul_munim@astro.com.my>
2019-01-24 23:55:44 +0800

    Define a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='bongotest',
+    version='1.0',
+    description='',
+    author='Munim Dibosh',
+    packages=['bongotest']
+)
